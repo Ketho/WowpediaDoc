08e21f91b85e67113bee7cce0cc84b6d78eb7fbd
Ketho <sghs17@outlook.com>
2020-04-24 23:14:52 +0200

    Refactoring

--- Wowpedia/Tables.lua
+++ Wowpedia/Tables.lua
@@ -10,43 +10,31 @@ function Wowpedia:GetTableText(apiTable, isTemplate)
 	local fullName = apiTable:GetFullName()
 	table.insert(tbl, '{| class="sortable darktable zebra" style="margin-left: 2em"')
 	if isTemplate then
-		table.insert(tbl, string.format('|+ {{#if:{{{nocaption|}}}||[[%s|%s]]}}', transclude, fullName))
+		table.insert(tbl, string.format("|+ {{#if:{{{nocaption|}}}||[[%s|%s]]}}", transclude, fullName))
 	else
-		table.insert(tbl, string.format('<!-- |+ [[%s|%s]] -->', transclude, fullName))
+		table.insert(tbl, string.format("<!-- |+ [[%s|%s]] -->", transclude, fullName))
 	end
 	if apiTable.Type == "Enumeration" then
 		table.insert(tbl, "! Value !! Key !! Description")
-		table.insert(tbl, self:GetTableRows(apiTable, '|-\n| align="center" | %s || %s ||'))
+		for _, field in ipairs(apiTable.Fields) do
+			table.insert(tbl, string.format('|-\n| align="center" | %s || %s ||', field.EnumValue, field.Name))
+		end
 	elseif apiTable.Type == "Structure" then
 		table.insert(tbl, "! Key !! Type !! Description")
-		table.insert(tbl, self:GetTableRows(apiTable, '|-\n| %s || %s ||'))
+		for _, field in ipairs(apiTable.Fields) do
+			local prettyType = self:GetPrettyType(field)
+			table.insert(tbl, string.format('|-\n| %s || %s ||', field.Name, prettyType))
+		end
 	elseif apiTable.Type == "Constants" then
 		table.insert(tbl, "! Constant !! Type !! Value !! Description")
-		table.insert(tbl, self:GetTableRows(apiTable, '|-\n| %s || %s || align="center" | %s ||'))
+		for _, field in ipairs(apiTable.Values) do
+			local prettyType = self:GetPrettyType(field)
+			table.insert(tbl, string.format('|-\n| %s || %s || align="center" | %s ||', field.Name, prettyType, field.Value))
+		end
 	end
 	table.insert(tbl, "|}")
 	local text = table.concat(tbl, "\n")
 	return isTemplate and string.format("{{wowapitype}}\n<onlyinclude>%s</onlyinclude>", text) or text
-end
-
-function Wowpedia:GetTableRows(apiTable, row)
-	local rows = {}
-	if apiTable.Type == "Enumeration" then
-		for i, field in ipairs(apiTable.Fields) do
-			rows[i] = row:format(field.EnumValue, field.Name)
-		end
-	elseif apiTable.Type == "Structure" then
-		for i, field in ipairs(apiTable.Fields) do
-			local prettyType = self:GetPrettyType(field)
-			rows[i] = row:format(field.Name, prettyType)
-		end
-	elseif apiTable.Type == "Constants" then
-		for i, field in ipairs(apiTable.Values) do
-			local prettyType = self:GetPrettyType(field)
-			rows[i] = row:format(field.Name, prettyType, field.Value)
-		end
-	end
-	return table.concat(rows, "\n")
 end
 
 function Wowpedia:GetTranscludeBase(complexTable)
