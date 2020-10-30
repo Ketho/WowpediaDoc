9042a1538606f2c67c24a101089f2b5ffab6389c
Ketho <sghs17@outlook.com>
2020-10-30 22:09:42 +0100

    SoulbindConduitTransactionType note

--- FrameXML/MissingDocumentation.lua
+++ FrameXML/MissingDocumentation.lua
@@ -63,7 +63,7 @@ local Missing =
 		{ Name = "RuneforgeLegendaryComponentInfo", Type = "Structure", Fields = Empty }, -- LegendaryCraftingDocumentation.lua
 		{ Name = "RuneforgeLegendaryCraftDescription", Type = "Structure", Fields = Empty }, -- LegendaryCraftingDocumentation.lua
 		{ Name = "RuneforgePower", Type = "Structure", Fields = Empty }, -- LegendaryCraftingDocumentation.lua
-		{ Name = "SoulbindConduitTransactionType", Type = "Enumeration", Fields = Empty }, -- CurrencyConstantsDocumentation.lua
+		{ Name = "SoulbindConduitTransactionType", Type = "Enumeration", Fields = Empty }, -- CurrencyConstantsDocumentation.lua; beta 9.0.2 (36401)
 		{ Name = "SoulbindConduitType", Type = "Enumeration", Fields = Empty }, -- SoulbindsDocumentation.lua
 		{ Name = "SoulbindNodeState", Type = "Enumeration", Fields = Empty }, -- SoulbindsDocumentation.lua
 		{ Name = "TradeSkillRecipeInfo", Type = "Structure", Fields = Empty }, -- TradeSkillUIDocumentation.lua
