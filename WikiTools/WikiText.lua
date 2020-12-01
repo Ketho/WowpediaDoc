d3b9150aefe6b7950c65fa5fac37e2859f533ffd
Ketho <sghs17@outlook.com>
2020-12-01 18:36:31 +0100

    Only show first 6 return values

--- WikiTools/WikiText.lua
+++ WikiTools/WikiText.lua
@@ -15,6 +15,14 @@ local function GetFullName(func)
 	return str
 end
 
+local function TrimReturnString(limit, s)
+	local pos = 1
+	for i = 1, limit do
+		pos = s:find(",", pos) + 1
+	end
+	return s:sub(1, pos-2)..", ..."
+end
+
 function WikiText:GetSignatures()
 	local t = {}
 	local fs_base = ': [[API %s|%s]](%s)%s'
@@ -27,7 +35,13 @@ function WikiText:GetSignatures()
 			args = fs_args:format(Wowpedia:GetSignature(func, "Arguments"))
 		end
 		if func.Returns then
-			returns = fs_returns:format(func:GetReturnString(false, false))
+			local returnString = func:GetReturnString(false, false)
+			if #returnString > 150 then -- limit too many return values
+				local shortRetStr = TrimReturnString(6, returnString)
+				returns = fs_returns:format(shortRetStr)
+			else
+				returns = fs_returns:format(returnString)
+			end
 		end
 		t[name] = fs_base:format(name, name, args, returns)
 	end
