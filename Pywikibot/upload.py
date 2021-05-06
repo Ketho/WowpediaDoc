7ea2a531e780ac39ea6bc273cb9d7b150208ebe6
Ketho <sghs17@outlook.com>
2021-05-06 12:11:03 +0200

    added upload script

--- Pywikibot/upload.py
+++ Pywikibot/upload.py
@@ -1,8 +1,9 @@
+#pylint: disable = import-error
 import pywikibot
 import os
 
 site = pywikibot.Site('en', 'wowpedia')
-PATH = "wowpedia"
+PATH = r"D:\Dev\Repo\wow-dev\WowpediaApiDoc\out"
 
 def getFileText(p):
 	f = open(p)
@@ -11,11 +12,11 @@ def getFileText(p):
 
 def saveFile(path, fileName):
 	name = fileName.replace(".txt", "")
-	print("-- reading "+name)
+	# print("-- reading "+name)
 	page = pywikibot.Page(site, name)
 	if not page.text:
 		page.text = getFileText(path)
-		page.save(summary="9.0.2")
+		page.save(summary="9.0.5")
 
 def recursiveFiles(path):
 	for base in os.listdir(path):
