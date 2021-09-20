e4027814c52cc914a97b6a109f38d8c12a47ec77
Ketho <sghs17@outlook.com>
2021-09-20 22:39:38 +0200

    Diff api params

--- Projects/ApiDocsDiff/ApiDocsDiff.lua
+++ Projects/ApiDocsDiff/ApiDocsDiff.lua
@@ -9,39 +9,42 @@ local apiTypes = {
 		map = function(tbl)
 			local t = {}
 			for _, system in pairs(tbl) do
-				for _, func in pairs(system.Functions or {}) do
+				for _, apiTable in pairs(system.Functions or {}) do
 					if system.Namespace then
-						local fullName = string.format("%s.%s", system.Namespace, func.Name)
-						t[fullName] = true
+						local fullName = string.format("%s.%s", system.Namespace, apiTable.Name)
+						t[fullName] = apiTable
 					else
-						t[func.Name] = true
+						t[apiTable.Name] = apiTable
 					end
 				end
 			end
 			return t
 		end,
+		params = {"Arguments", "Returns"},
 	},
 	Events = {
 		map = function(tbl)
 			local t = {}
 			for _, system in pairs(tbl) do
-				for _, event in pairs(system.Events or {}) do
-					t[event.LiteralName] = true
+				for _, apiTable in pairs(system.Events or {}) do
+					t[apiTable.LiteralName] = apiTable
 				end
 			end
 			return t
 		end,
+		params = {"Payload"},
 	},
 	Tables = {
 		map = function(tbl)
 			local t = {}
 			for _, system in pairs(tbl) do
-				for _, structure in pairs(system.Tables or {}) do
-					t[structure.Name] = structure
+				for _, apiTable in pairs(system.Tables or {}) do
+					t[apiTable.Name] = apiTable
 				end
 			end
 			return t
 		end,
+		params = {"Fields"},
 	},
 }
 
@@ -52,9 +55,9 @@ local m = {}
 function m:main()
 	local versions = self:GetApiDocVersions("FrameXML/retail")
 	local framexml = self:LoadFrameXML(versions)
-	-- self:CompareVersions(framexml, "9.0.5.38556", "9.1.0.40000")
+	self:CompareVersions(framexml, "9.0.5.38556", "9.1.0.40000")
 	-- self:CompareVersions(framexml, "9.1.0.40000", "9.1.5.40071")
-	self:CompareVersions(framexml, "9.0.5.38556", "9.1.5.40071")
+	-- self:CompareVersions(framexml, "9.0.1.36577", "9.1.5.40071")
 end
 
 function m:GetApiDocVersions(path)
@@ -84,12 +87,15 @@ end
 function m:CompareVersions(tbl, a, b)
 	print(string.format("Comparing %s to %s ", a, b))
 	for _, apiType in pairs(apiType_order) do
-		print("# "..apiType)
-		self:CompareApiTypes(tbl[a][apiType], tbl[b][apiType])
+		local left = tbl[a][apiType]
+		local right = tbl[b][apiType]
+		print("* "..apiType)
+		local paramTblNames = apiTypes[apiType].params
+		self:CompareApiTables(left, right, paramTblNames)
 	end
 end
 
-function m:CompareApiTypes(a, b)
+function m:CompareApiTables(a, b, paramTblNames)
 	for _, name in pairs(Util:SortTable(b)) do
 		if not a[name] then
 			print("+", name)
@@ -100,6 +106,39 @@ function m:CompareApiTypes(a, b)
 			print("-", name)
 		end
 	end
+	for _, name in pairs(Util:SortTable(b)) do
+		if a[name] and b[name] then
+			for _, paramTblName in pairs(paramTblNames) do
+				local leftParam, rightParam = a[name][paramTblName], b[name][paramTblName]
+				if (leftParam and not rightParam) or (not leftParam and rightParam) then
+					print("#!", name, paramTblName)
+				elseif leftParam and rightParam then
+					local equals = self:CrappyTableEquals(leftParam, rightParam)
+					if not equals then
+						print("#", name, paramTblName)
+					end
+				end
+			end
+		end
+	end
+end
+
+function m:CrappyTableEquals(a, b)
+	if #a ~= #b then
+		return false
+	end
+	for k, v in pairs(a) do
+		if v.Name ~= b[k].Name then
+			return false
+		end
+	end
+	for k, v in pairs(b) do
+		if v.Name ~= a[k].Name then
+			return false
+		end
+	end
+	return true
 end
 
 m:main()
+print("done")
