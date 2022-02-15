6996952a26d6aa22f1f92a84a98d89ce9a22efcc
Ketho <sghs17@outlook.com.com>
2022-02-15 08:52:15 +0100

    Added progenitor language atlases

---
+++ Projects/progenitorlang/main.py
@@ -0,0 +1,32 @@
+import AtlasInfo
+from pathlib import Path
+from PIL import Image
+
+imgPath = Path("Projects", "progenitorlang", "img")
+
+colors = {
+	"black": ["webp", "black"],
+	"white": ["png", "white"],
+	"blackdarkbrown": ["png", "blackdarkbrown"],
+	"broadcastyellow": ["png", "broadcastyellow"],
+	"normal": ["png", ""],
+}
+
+def read_image(folder, info):
+	ext, color = info
+	im = Image.open(Path(imgPath, folder, f"ingamelanguagesprogenitor{color}.{ext}"))
+	texture = AtlasInfo.data[f"interface/ingamelanguagues/ingamelanguagesprogenitor{color}"]
+	imWidth, imHeight = im.size
+	for atlas, v in texture.items():
+		left, right, top, bottom = v[2:]
+		crop = im.crop((left*imWidth, top*imHeight, right*imWidth, bottom*imHeight))
+		print(crop)
+		crop.save(Path(imgPath, folder, atlas+".png"))
+
+def main():
+	for folder, info in colors.items():
+		read_image(folder, info)
+	print("done")
+
+if __name__ == '__main__':
+	main()
