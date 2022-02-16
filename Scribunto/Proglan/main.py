1ee7bf7378ffe181a8388eaa9dce46aae66fd6d1
Ketho <sghs17@outlook.com.com>
2022-02-16 06:57:10 +0100

    Refactor proglan script Read from dir and save crops to subfolder

--- Scribunto/Proglan/main.py
+++ Scribunto/Proglan/main.py
@@ -1,30 +1,25 @@
+import os
 import AtlasInfo
 from pathlib import Path
 from PIL import Image
 
 imgPath = Path("Scribunto", "Proglan", "img")
 
-colors = {
-	"black": ["webp", "black"],
-	"blackdarkbrown": ["png", "blackdarkbrown"],
-	"broadcastyellow": ["png", "broadcastyellow"],
-	"normal": ["png", ""],
-	"white": ["png", "white"],
-}
-
-def read_image(folder, info):
-	ext, color = info
-	im = Image.open(Path(imgPath, folder, f"ingamelanguagesprogenitor{color}.{ext}"))
+def read_image(path):
+	im = Image.open(path)
 	imWidth, imHeight = im.size
-	texture = AtlasInfo.data[f"interface/ingamelanguagues/ingamelanguagesprogenitor{color}"]
+	texture = AtlasInfo.data[f"interface/ingamelanguagues/{path.stem}"]
 	for atlas, v in texture.items():
 		left, right, top, bottom = v[2:]
 		crop = im.crop((left*imWidth, top*imHeight, right*imWidth, bottom*imHeight))
-		crop.save(Path(imgPath, folder, atlas+".png"))
+		crop.save(Path(path.parent, path.stem, atlas+".png"))
 
 def main():
-	for folder, info in colors.items():
-		read_image(folder, info)
+	for file in os.listdir(imgPath):
+		path = Path(imgPath, file)
+		if path.is_file() and not Path(imgPath, path.stem).exists():
+			os.mkdir(Path(imgPath, path.stem))
+			read_image(path)
 	print("done")
 
 if __name__ == '__main__':
--- Scribunto/Proglan/upload.py
+++ Scribunto/Proglan/upload.py
@@ -8,13 +8,13 @@ imgPath = Path("Scribunto", "Proglan", "img")
 site = pywikibot.Site('en', 'wowpedia')
 pageText = "[[Category:WoW Icons: Progenitor Language]]"
 
-def uploadFile(path, name):
-	page = pywikibot.Page(site, "File:"+name)
+def uploadFile(path):
+	page = pywikibot.Page(site, "File:"+path.name)
 	if not page.exists():
-		print("uploading", name)
+		print("uploading", path.name)
 		site.upload(
 			filepage = page,
-			source_filename = Path(path, name),
+			source_filename = path,
 			text = pageText,
 			comment = "progenitorlang symbols",
 		)
@@ -22,9 +22,9 @@ def uploadFile(path, name):
 def main():
 	for folder in os.listdir(imgPath):
 		subPath = Path(imgPath, folder)
-		for file in os.listdir(subPath):
-			if not "ingamelanguagesprogenitor" in file:
-				uploadFile(subPath, file)
+		if subPath.is_dir():
+			for file in os.listdir(subPath):
+				uploadFile(Path(subPath, file))
 	print("done")
 
 if __name__ == '__main__':
