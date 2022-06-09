a1ba29a91c43abd318e8761a9873306ec28c22b0
Ketho <sghs17@outlook.com>
2022-06-09 16:04:16 +0200

    Sync descriptions

---
+++ Pywikibot/descriptions/update_wiki.py
@@ -0,0 +1,44 @@
+import re
+import pywikibot
+import data
+
+# def find_desc(page):
+# 	l = page.text.splitlines()
+# 	if "<!--desc-->" in page.text:
+# 		for line in l:
+# 			m = re.match("<!--desc-->(.+)", line)
+# 			if m:
+# 				return m.group(1)
+# 	else:
+# 		return l[1]
+
+def update_desc(page):
+	title = page.title().replace("API ", "")
+	title = title.replace("C ", "C_")
+	l = page.text.splitlines()
+	if "<!--desc-->" in page.text:
+		for i, line in enumerate(l):
+			m = re.match("<!--desc-->(.+)", line)
+			if m:
+				l[i] = "<!--desc-->"+data.descriptions[title]
+	else:
+		l[1] = data.descriptions[title]
+	return str.join("\n", l)
+
+def main():
+	site = pywikibot.Site("en", "wowpedia")
+	for k in data.descriptions:
+		page = pywikibot.Page(site, f"API {k}")
+		# desc = find_desc(page)
+		# if desc:
+		# 	print(page.title(underscore=False)+": "+desc)
+		# else:
+		# 	print(page.full_url())
+		newText = update_desc(page)
+		if page.text != newText:
+			page.text = newText
+			page.save("Sync description")
+
+if __name__ == "__main__":
+	main()
+	print("done")
---
+++ Pywikibot/descriptions/write_data.lua
@@ -0,0 +1,15 @@
+local Util = require("Util/Util")
+package.path = package.path..";../?.lua"
+local updated_desc = require("wow-api-descriptions/updated")
+local OUT = "Pywikibot/descriptions/data.py"
+
+local fs = '\t"%s": "%s",\n'
+
+local file = io.open(OUT, "w")
+file:write("descriptions = {\n")
+for _, k in pairs(Util:SortTable(updated_desc)) do
+	local v = updated_desc[k]:gsub([["]], [[\"]])
+	file:write(fs:format(k, v))
+end
+file:write("}\n")
+file:close()
