815af58df760e9419ed1a2294473a19547b3f955
Ketho <sghs17@outlook.com>
2022-07-01 02:06:42 +0200

    Fix flavor script

--- Projects/UpdateResources/UpdateResources.lua
+++ Projects/UpdateResources/UpdateResources.lua
@@ -1,12 +1,12 @@
 local Util = require("Util/Util")
 
--- mainline, tbc, vanilla
+-- mainline, ptr, tbc, vanilla
 local FLAVOR = "mainline"
 local options = Util:GetFlavorOptions(FLAVOR)
 require("Projects.UpdateResources.GlobalStrings")(options)
 require("Projects.UpdateResources.AtlasInfo")(options)
 
 local DumbXmlParser = require("Projects.DumbXmlParser.DumbXmlParser")
-DumbXmlParser:main(FLAVOR)
+DumbXmlParser:main(options.flavor)
 
 print("done")
