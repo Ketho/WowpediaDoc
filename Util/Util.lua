ec00c8185a7d610a68829cfb62bd3f30d61fa683
Ketho <sghs17@outlook.com>
2022-10-15 22:48:27 +0200

    mainline_beta

--- Util/Util.lua
+++ Util/Util.lua
@@ -16,10 +16,9 @@ end
 Util.PtrVersion = "10.0.0"
 
 local flavorInfo = {
-	ptr = {flavor = "mainline", header = true,
-		sort = Util.SortBuild},
-	mainline = {flavor = "mainline", header = true, build = "9.2.7.",
-		sort = Util.SortBuild},
+	mainline_beta = {flavor = "mainline", header = true, build = "10.0.2.", sort = Util.SortBuild},
+	mainline_ptr = {flavor = "mainline", header = true, build = "10.0.0.", sort = Util.SortBuild},
+	mainline = {flavor = "mainline", header = true, build = "9.2.7.", sort = Util.SortBuild},
 	tbc = {flavor = "tbc", header = true, build = "2.5.4."},
 	wrath = {flavor = "wrath", header = true, build = "3.4.0."},
 	vanilla = {flavor = "vanilla", header = true, build = "1.14.3."},
