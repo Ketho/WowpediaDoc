3ac3c16907c552f23c02889fd504472c4b826256
Ketho <sghs17@outlook.com>
2022-10-18 16:21:20 +0200

    download before updating

--- Pywikibot/work/replace.py
+++ Pywikibot/work/replace.py
@@ -1,11 +1,15 @@
 import re
 import util.wowpedia
 
+import sys
+sys.path.append('Pywikibot/export')
+import parse_html
+
 def update_text(name: str, s: str):
 	l = s.splitlines()
 	isUpdate = False
-	a = ']]?}}'
-	b = ']]?'
+	a = '{{apitype|string}} : {{:ItemInfo}}'
+	b = '{{apitype|number,string}} : {{:ItemInfo}}'
 	for i, v in enumerate(l):
 		if a in v:
 			l[i] = v.replace(a, b)
@@ -14,7 +18,8 @@ def update_text(name: str, s: str):
 		return str.join("\n", l)
 
 def main():
-	util.wowpedia.main(update_text, summary="fix mixin formatting")
+	parse_html.main()
+	util.wowpedia.main(update_text, summary="number|string")
 
 if __name__ == "__main__":
 	main()
