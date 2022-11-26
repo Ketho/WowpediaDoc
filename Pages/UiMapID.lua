c850a5d768340637cc7a72c39ac4e202d6d4211c
Ketho <sghs17@outlook.com>
2022-11-26 15:04:09 +0100

    uimap classic

---
+++ Pages/UiMapID.lua
@@ -0,0 +1,26 @@
+-- https://wowpedia.fandom.com/wiki/UiMapID#Classic
+local Util = require("Util/Util")
+local parser = require("Util/wowtoolsparser")
+local wowpedia_export = require("Util/wowpedia_export")
+local dbc_patch = require("Projects/DBC/DBC_patch")
+
+local OUTPUT = "KethoWowpedia/patch/uimap.lua"
+
+local function main(options)
+	options = Util:GetFlavorOptions(options)
+	local patchData = dbc_patch:GetPatchData("uimap", options)
+	print("writing to "..OUTPUT)
+	local file = io.open(OUTPUT, "w")
+
+	local fs = '\t[%d] = "%s",\n'
+	file:write("KethoWowpedia.patch.uimap = {\n")
+	Util:ReadCSV("uimap", parser, options, function(_, ID, l)
+		local patch = patchData[ID] and Util:GetPatchVersion(patchData[ID]) or ""
+		file:write(fs:format(ID, patch))
+	end)
+	file:write("}\n")
+	file:close()
+end
+
+main("wrath")
+print("done")
