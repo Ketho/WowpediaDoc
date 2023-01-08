efadfe316992872b09fcdccc3e972678f31d1937
Ketho <sghs17@outlook.com>
2023-01-08 15:31:41 +0100

    update widget html

--- Scribunto/API_info/wowprog/widgets/widgets.lua
+++ Scribunto/API_info/wowprog/widgets/widgets.lua
@@ -1,20 +1,32 @@
 -- https://wowprogramming.com/docs/widgets.html
 -- https://wowpedia.fandom.com/wiki/Module:API_info/wowprog/widgets
 local Util = require("Util/Util")
+local gumbo = require("gumbo")
 
-local file = io.open("Scribunto/API_info/wowprog/widgets/widgets.html", "r")
+local path = "Scribunto/API_info/wowprog/widgets/widgets.html"
+local file = io.open(path, "r")
 local text = file:read("a")
 
-local t = {}
+-- some links are not red but are empty undocumented pages anyway
+local undoc = {}
+for s in text:gmatch('(%w+:%w+)%(%)</a> %- <em>This function is not yet documented</em>') do
+	undoc[s] = true
+end
+file:close()
 
-for s in text:gmatch('<a href="(.-)"') do
-	local widget_obj = s:match("docs/widgets/(%w-)%.html")
-	if widget_obj then
-		t[widget_obj] = true
-	end
-	local widget, method = s:match("docs/widgets/(%w-)/(%w-)%.html")
-	if widget then
-		t[widget..":"..method] = true
+local document = assert(gumbo.parseFile(path))
+
+local t = {}
+for i, element in ipairs(document.links) do
+	local href = element:getAttribute("href")
+	local class = element:getAttribute("class")
+	if href:find("/docs/widgets/%w+/") and not class then
+		local widget, method = href:match("widgets/(%w+)/(%w+)")
+		local fullName = widget..":"..method
+		-- seems to be just SetSmoothProgress and GetDontSavePosition
+		if not undoc[fullName] then
+			t[widget..":"..method] = true
+		end
 	end
 end
 
