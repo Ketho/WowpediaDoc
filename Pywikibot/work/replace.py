b6432e1fb798049931eb69b285655d1307e2c0f9
Ketho <sghs17@outlook.com>
2023-01-28 23:05:23 +0100

    don't remember why I looped over the lines

--- Pywikibot/work/replace.py
+++ Pywikibot/work/replace.py
@@ -6,19 +6,14 @@ sys.path.append('Pywikibot/export')
 import parse_html
 
 def update_text(name: str, s: str):
-	l = s.splitlines()
-	isUpdate = False
-	a = '}} || Default = 0'
-	b = '|default=0}} || '
-	for i, v in enumerate(l):
-		if a in v:
-			l[i] = v.replace(a, b)
-			isUpdate = True
-	if isUpdate:
-		return str.join("\n", l)
+	a = 'hello'
+	b = 'world'
+	if a in s:
+		s = s.replace(a, b)
+		return s
 
 def main():
-	util.wowpedia.main(update_text, summary="default")
+	util.wowpedia.main(update_text, summary="something")
 	parse_html.main()
 
 if __name__ == "__main__":
