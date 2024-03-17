8a6c6452f21d37f0cdddac13574148c834d8a1be
Ketho <sghs17@outlook.com>
2024-03-17 11:33:02 +0100

    add wowprog events

---
+++ Scribunto/API_info/wowprog/events/events.lua
@@ -0,0 +1,30 @@
+-- https://wowprogramming.com/docs/events.html
+-- https://web.archive.org/web/20170519220226id_/http://wowprogramming.com/docs/events
+-- https://warcraft.wiki.gg/wiki/Module:API_info/wowprog/events
+local gumbo = require "gumbo"
+local document = gumbo.parseFile("Scribunto/API_info/wowprog/events/events.html")
+
+local Util = require("Util/Util")
+
+local class = document:getElementsByClassName("event-hack")
+local api_elements = class[1].children[1]
+
+local t = {}
+
+for _, v in pairs(api_elements.children) do
+	if type(v) == "table" then
+		local el = v.children[1].children[1]
+		if el then
+			local description = v.children[2].textContent
+			if not description:find("This event is not yet documented") then
+				local name = el.textContent
+				-- local url_part = el:getAttribute("href")
+				t[name] = true
+			end
+		end
+	end
+end
+
+for _, v in pairs(Util:SortTable(t)) do
+	print(v)
+end
