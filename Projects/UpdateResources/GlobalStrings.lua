8cc3e6c9a19f6023b6f12b86ab5d1e9fba7b897f
Ketho <sghs17@outlook.com>
2024-04-12 17:52:47 +0200

    KEY_OEM5 hack

--- Projects/UpdateResources/GlobalStrings.lua
+++ Projects/UpdateResources/GlobalStrings.lua
@@ -78,7 +78,7 @@ local function GlobalStrings(path, options)
 		value = value:gsub([[\"]], [["]]) -- unescape any quotes
 		value = value:gsub([["]], [[\"]]) -- before escaping quotes
 		value = FixEscapes(value)
-		if key == "KEY_BACKSLASH" then
+		if key == "KEY_BACKSLASH" or key == "KEY_OEM5" then
 			value = [[\\]]
 		end
 		if options.locale == "esES" and key == "ABANDON_QUEST_CONFIRM" then
